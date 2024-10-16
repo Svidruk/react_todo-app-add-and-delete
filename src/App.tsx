035c35f3a18da6e1e96247d480e88ab2a7eb33af
Svidruk <mr.svid999@gmail.com>
2024-10-16 21:12:55 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,107 @@
-/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import React, { useEffect, useState } from 'react';
+import { addTodo, getTodos, deleteTodo } from './api/todos';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { ErrorNotification } from './components/ErrorNotification';
+import { Todo } from './types/Todo';
+import { ErrorMessages } from './types/ErrorMessages';
+import { FilterOptions } from './types/FilterOptions';
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filterOption, setFilterOption] = useState<FilterOptions>(
+    FilterOptions.DEFAULT,
+  );
+  const [errorMessage, setErrorMessage] = useState<ErrorMessages>(
+    ErrorMessages.NO_ERROR,
+  );
+  const [newTitle, setNewTitle] = useState('');
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [isReceivingAnswer, setIsReceivingAnswer] = useState(false);
+
+
+  const handleErrorReset = () => {
+    setErrorMessage(ErrorMessages.NO_ERROR);
+  };
+
+  const handleErrorMessage = (error: ErrorMessages) => {
+    setErrorMessage(error);
+    setTimeout(handleErrorReset, 3000);
+  };
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => {
+        handleErrorMessage(ErrorMessages.UNABLE_TO_LOAD_TODO);
+      });
+  }, []);
+
+  const handleAddTodo = (newTodo: Todo) => {
+    setTempTodo(newTodo);
+    setIsReceivingAnswer(true);
+
+    addTodo(newTodo)
+      .then(todo => {
+        setTodos(currentTodos => [...currentTodos, todo]);
+        setNewTitle('')
+      })
+      .catch(() => {
+        handleErrorMessage(ErrorMessages.UNABLE_TO_ADD_TODO);
+      })
+      .finally(() => {
+        setTempTodo(null);
+        setIsReceivingAnswer(false);
+      });
+  };
+
+  const handleTodoDelete = (id: number) => {
+    deleteTodo(id)
+      .then(() =>
+        setTodos(currentTodos => currentTodos.filter(todo => todo.id !== id)),
+      )
+      .catch(() => {
+        handleErrorMessage(ErrorMessages.UNABLE_TO_DELETE_TODO);
+      });
+  };
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header
+          todos={todos}
+          onAddTodo={handleAddTodo}
+          onCheckForErrors={handleErrorMessage}
+          receivingAnswer={isReceivingAnswer}
+          newTitle={newTitle}
+          setNewTitle={setNewTitle}
+        />
+
+        <TodoList
+          tempTodo={tempTodo}
+          todos={todos}
+          filterOption={filterOption}
+          onTodoDelete={handleTodoDelete}
+        />
+
+        {todos.length > 0 && (
+          <Footer
+            todos={todos}
+            currentFilter={filterOption}
+            onFilterOptionChange={setFilterOption}
+          />
+        )}
+      </div>
+
+      <ErrorNotification
+        errorMessage={errorMessage}
+        onResetError={handleErrorReset}
+      />
+    </div>
   );
 };
